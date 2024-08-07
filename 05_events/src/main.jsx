e969f623b35f14317f02fe376458ab88ae1500a9
rapPayne <bob@creator.net>
2024-08-07 13:33:06 -0500

    Rearranged order of styling and events

--- 05_events/src/main.jsx
+++ 05_events/src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './main.css'
-import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
